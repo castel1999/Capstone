47c87f24057e19bb4c751aa270464a1e289712b1
ldduc123 <ducld0510@gmail.com>
2024-05-18 13:34:37 +0700

    feat: avatar upload firebase

---
+++ src/pages/tutorList/SideBarTutorListPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideBarTutorListPage = () => {
+  return (
+    <div>
+        <div className='text-5xl font-semibold'>Học với gia sư</div>
+    </div>
+  )
+}
+
+export default SideBarTutorListPage
