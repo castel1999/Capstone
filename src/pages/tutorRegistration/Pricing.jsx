db59b46c83dd95c7b9d8d333ebd677a4e4ceb9cd
Hi-Im-Slag <105272655+Hi-Im-Slag@users.noreply.github.com>
2024-06-07 17:00:25 +0700

    Add stage 5 and 6

--- src/pages/tutorRegistration/Pricing.jsx
+++ src/pages/tutorRegistration/Pricing.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
-const Pricing = () => {
+const Pricing = (props) => {
+  const setStage = props.setStage;
+
+  const handleGoback = () => {
+    setStage(5);
+  };
+
+  const handleSubmit = () => {
+    alert('register finish')
+  };
+
   return (
-    <div>Pricing</div>
-  )
-}
+    <div className="flex flex-col p-12 gap-6">
+      <div className="text-[32px] font-semibold">
+        Đặt mức giá thuê hàng giờ của bạn
+      </div>
+      <input
+        className="px-[14px] py-[10px] border-2 rounded-lg focus:outline-none focus:ring-0 focus:border-[#6B48F2] hover:border-black"
+        type="number"
+        onChange={(e) => {}}
+        min={0}
+        step={1000}
+        required
+      />
 
-export default Pricing
+<div className="flex flex-row-reverse items-end gap-2">
+        <div
+          onClick={handleSubmit}
+          className="px-6 py-2 border-2 border-black rounded-md bg-theme hover:bg-[#7E5FF4] text-white font-semibold cursor-pointer"
+        >
+          Hoàn thành
+        </div>
+        <div
+          onClick={handleGoback}
+          className="px-6 py-2 text-center border-2 border-[#dcdce5] rounded-md w-fit hover:bg-[rgba(18,17,23,.06)] cursor-pointer"
+        >
+          Trở lại
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
