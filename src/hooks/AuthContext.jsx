dd5f3c408c271a9a04108ed318c32e852b8948c5
ldduc123 <ducld0510@gmail.com>
2024-06-09 00:32:31 +0700

    fix: token exp

--- src/hooks/AuthContext.jsx
+++ src/hooks/AuthContext.jsx
@@ -1,21 +1,63 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
+import {jwtDecode} from "jwt-decode";
 
 const AuthContext = createContext();
+
+const getRemainingTime = (exp) => {
+  const currentTime = Date.now();
+  const expirationTime = exp * 1000; // convert exp to milliseconds
+  return expirationTime - currentTime;
+};
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(() => {
     const token = localStorage.getItem("token");
+    if (!token) return null;
+
+    const decodedToken = jwtDecode(token);
     const role = localStorage.getItem("role");
     const userId = localStorage.getItem("userID");
-    return token && role && userId ? { role, userId } : null;
+
+    if (decodedToken.exp * 1000 < Date.now()) {
+      localStorage.clear();
+      return null;
+    }
+
+    return { role, userId, token, decodedToken };
   });
+
+  useEffect(() => {
+    if (!user) return;
+
+    const remainingTime = getRemainingTime(user.decodedToken.exp);
+    const timer = setTimeout(() => {
+      localStorage.clear();
+      setUser(null);
+      window.location.href = "/login"; // Redirect to login page
+    }, remainingTime);
+
+    return () => clearTimeout(timer);
+  }, [user]);
 
   useEffect(() => {
     const handleStorageChange = () => {
       const token = localStorage.getItem("token");
+      if (!token) {
+        setUser(null);
+        return;
+      }
+
+      const decodedToken = jwtDecode(token);
       const role = localStorage.getItem("role");
       const userId = localStorage.getItem("userID");
-      setUser(token && role && userId ? { role, userId } : null);
+
+      if (decodedToken.exp * 1000 < Date.now()) {
+        localStorage.clear();
+        setUser(null);
+        return;
+      }
+
+      setUser({ role, userId, token, decodedToken });
     };
 
     window.addEventListener("storage", handleStorageChange);
