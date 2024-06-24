2cb45f3bcd9ddd3d507a7a89496693f3d53753d8
ldduc123 <ducld0510@gmail.com>
2024-06-24 18:20:38 +0700

    fix: package json conflict

--- src/pages/settings/wallet/WalletPage.jsx
+++ src/pages/settings/wallet/WalletPage.jsx
@@ -199,7 +199,7 @@ const WalletPage = () => {
       <div className="mb-6 w-fit">
         <div className="flex flex-row gap-5 border-2 border-black rounded-lg justify-center">
           <div className="flex flex-col gap-2 px-5 py-7">
-            <div className="text-2xl font-semibold">Total Balance</div>
+            <div className="text-2xl font-semibold">Tổng số tiền</div>
             <div>
               <div className="text-green-500 text-2xl font-semibold tracking-widest">
                 {lastTransaction?formatCurrency(lastTransaction?.amount):formatCurrency(0)}
